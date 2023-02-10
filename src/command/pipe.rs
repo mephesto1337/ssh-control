afeb2e43c0b8b04763e0f30e88de597f565c7a08
Thomas WACHE <thomas@wache.fr>
2023-02-10 10:55:23 +0100

    Added std::process::Command like interface

---
+++ src/command/pipe.rs
@@ -0,0 +1,102 @@
+use std::{
+    fs,
+    io::{self, Read, Write},
+    mem::MaybeUninit,
+    os::{
+        fd::IntoRawFd,
+        unix::io::{AsRawFd, RawFd},
+    },
+};
+
+#[derive(Debug)]
+pub struct PipeRead(pub(crate) RawFd);
+
+#[derive(Debug)]
+pub struct PipeWrite(pub(crate) RawFd);
+
+#[derive(Debug)]
+pub struct Pipe {
+    pub(crate) read: PipeRead,
+    pub(crate) write: PipeWrite,
+}
+
+impl Pipe {
+    pub fn new() -> io::Result<Self> {
+        let mut fds: MaybeUninit<[RawFd; 2]> = MaybeUninit::uninit();
+        let ret = unsafe { libc::pipe(fds.as_mut_ptr().cast()) };
+        if ret == 0 {
+            let fds = unsafe { fds.assume_init() };
+            Ok(Self {
+                read: PipeRead(fds[0]),
+                write: PipeWrite(fds[1]),
+            })
+        } else {
+            Err(io::Error::last_os_error())
+        }
+    }
+
+    pub fn dev_null() -> io::Result<Self> {
+        let r = fs::OpenOptions::new().read(true).open("/dev/null")?;
+        let w = fs::OpenOptions::new().write(true).open("/dev/null")?;
+        let read = PipeRead(r.into_raw_fd());
+        let write = PipeWrite(w.into_raw_fd());
+        Ok(Self { read, write })
+    }
+}
+
+impl AsRawFd for PipeRead {
+    fn as_raw_fd(&self) -> RawFd {
+        self.0
+    }
+}
+
+impl AsRawFd for PipeWrite {
+    fn as_raw_fd(&self) -> RawFd {
+        self.0
+    }
+}
+
+impl Read for PipeRead {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        let ret = unsafe { libc::read(self.0, buf.as_mut_ptr().cast(), buf.len()) };
+        if ret >= 0 {
+            Ok(ret as usize)
+        } else {
+            Err(io::Error::last_os_error())
+        }
+    }
+}
+
+impl Write for PipeWrite {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        let ret = unsafe { libc::write(self.0, buf.as_ptr().cast(), buf.len()) };
+        if ret >= 0 {
+            Ok(ret as usize)
+        } else {
+            Err(io::Error::last_os_error())
+        }
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        // There is no buffering
+        Ok(())
+    }
+}
+
+impl Drop for PipeRead {
+    fn drop(&mut self) {
+        let ret = unsafe { libc::close(self.0) };
+        if ret < 0 {
+            log::warn!("Could not close PipeRead {}", self.0);
+        }
+    }
+}
+
+impl Drop for PipeWrite {
+    fn drop(&mut self) {
+        let ret = unsafe { libc::close(self.0) };
+        if ret < 0 {
+            log::warn!("Could not close PipeWrite {}", self.0);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,42 @@
-use std::env;
+use std::{env, fs, io};
 
-use ssh_control::{Result, SshControl};
+use ssh_control::{
+    command::{Pipe, SshCommand},
+    Result, SshControl,
+};
 
-fn main() -> Result<()> {
-    env_logger::init();
+fn main() {
+    env_logger::builder()
+        .parse_default_env()
+        .target(env_logger::Target::Stderr)
+        .init();
 
+    if let Err(e) = main_helper() {
+        log::error!("{e}");
+    }
+}
+
+fn main_helper() -> Result<()> {
     let mut ctrl = SshControl::new(env::args().nth(1).unwrap())?;
-    ctrl.check_alive()?;
-    ctrl.new_session("id")?;
-    let msg = ctrl.recv()?;
-    dbg!(msg);
+    let server_pid = ctrl.check_alive()?;
+    log::info!("Server pid: {server_pid}");
+
+    let mut cmd = SshCommand::new("id");
+    cmd.stderr(Pipe::dev_null()?);
+    cmd.stdout(Pipe::new()?);
+
+    let mut f = fs::OpenOptions::new()
+        .create(true)
+        .write(true)
+        .append(false)
+        .open("/tmp/test")?;
+
+    let mut child = ctrl.new_session(cmd)?;
+
+    let n = io::copy(child.stdout.as_mut().unwrap(), &mut f)?;
+    log::info!("Copy {n} bytes from command to file");
+
+    ctrl.wait(&child)?;
 
     Ok(())
 }
